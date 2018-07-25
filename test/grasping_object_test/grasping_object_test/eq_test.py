b5296928be782e47d072581c51cd00e0ced30069
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-07-25 11:26:44 +0200

    Added equal test to grasping test object

---
+++ test/grasping_object_test/grasping_object_test/eq_test.py
@@ -0,0 +1,23 @@
+import unittest
+from grasping_object.grasping_object import GraspingObject
+from test.utils.test_object_generator import FACING_ROBOT_FACE, BOTTOM_FACE
+
+
+class Equals(unittest.TestCase):
+    def test_should_return_true_if_grasping_objects_are_equal(self):
+        grasping_object_1 = GraspingObject(FACING_ROBOT_FACE, BOTTOM_FACE)
+        grasping_object_2 = GraspingObject(FACING_ROBOT_FACE, BOTTOM_FACE)
+        actual = grasping_object_1 == grasping_object_2
+        self.assertEqual(actual, True)
+
+    def test_should_return_false_if_grasping_objects_are_not_equal(self):
+        grasping_object_1 = GraspingObject(FACING_ROBOT_FACE, BOTTOM_FACE)
+        grasping_object_2 = GraspingObject(BOTTOM_FACE, FACING_ROBOT_FACE)
+        actual = grasping_object_1 == grasping_object_2
+        self.assertEqual(actual, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
