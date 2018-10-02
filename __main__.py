064523aa36ca24c710e3a5a2943dcd7b90f8e0c8
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-10-02 15:57:55 +0200

    Added prototype pipeline for inference

--- __main__.py
+++ __main__.py
@@ -1,19 +1,27 @@
 from pracmln.mlnquery import MLNQuery
 from markov_logic_network.grasping_type_mln import grasping_type_pracmln_project, grasping_type_mln
-from markov_logic_network.database import Database
+from word_net import determine_path_similarity_between_two_concepts
+from grasping_object.grasping_object import GraspingObject
+from grasping_object.orientation import Orientation
 
 if __name__ == "__main__":
 
     project = grasping_type_pracmln_project
     query_config = project.queryconf
-    evidence_database = Database(grasping_type_mln)
-    evidence_database.add_ground_atom_with_truth_value('facing_robot_face(front)', 1.0)
-    evidence_database.add_ground_atom_with_truth_value('bottom_face(bottom)', 1.0)
-    evidence_database.add_ground_atom_with_truth_value('is_a(tankard.n.01, mug.n.01)', 0.66)
-    evidence_database.add_ground_atom_with_truth_value('is_a(tankard.n.01, cup.n.01)', 0.33)
-    evidence_database.add_ground_atom_with_truth_value('is_a(tankard.n.01, knife.n.01)', 0.1)
-    evidence_database.add_ground_atom_with_truth_value('obj_to_be_grasped(tankard.n.01)', 1.0)
 
-    mln_query = MLNQuery(query_config, mln=grasping_type_mln, db=evidence_database.pracmln_database)
+    orientation = Orientation('front', 'bottom')
+    grasping_object = GraspingObject('tankard.n.01', orientation)
+
+    evidence_database = grasping_object.transform_to_mln_database(grasping_type_mln)
+
+    for learned_object in grasping_type_mln.domains['learnedObject']:
+        similarity = determine_path_similarity_between_two_concepts(grasping_object.type, learned_object)
+        ground_atom = 'is_a({},{})'.format(grasping_object.type, learned_object)
+        evidence_database.add_ground_atom_with_truth_value(ground_atom, similarity)
+
+    mln_query = MLNQuery(query_config, mln=grasping_type_mln, db=evidence_database.pracmln_database, verbose=0)
+
     result = mln_query.run()
-    pass
+    for ground_atom in result.results.keys():
+        if result.results[ground_atom] == 1:
+            print ground_atom
