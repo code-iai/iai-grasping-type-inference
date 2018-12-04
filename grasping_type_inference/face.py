2c486a7798eb3348825630e2550228bd63dc66dc
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-12-04 10:01:45 +0100

    Added encoding to avoid import errors

--- grasping_type_inference/face.py
+++ grasping_type_inference/face.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright © 2018 Institute for Artificial Intelligence - University of Bremen
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
