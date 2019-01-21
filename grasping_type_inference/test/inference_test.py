25ccfda22b18e1090fff8e360aeae04c063c0138
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-01-21 09:30:03 +0100

    test(inference_test.py): adds test for testing the spoon inference

--- grasping_type_inference/test/inference_test.py
+++ grasping_type_inference/test/inference_test.py
@@ -12,6 +12,11 @@ def test_should_return_the_most_probable_result():
 
     assert 'TOP' == result
 
+    inference = Inference(':LEFT-SIDE', ':BOTTOM', 'spoon.n.01')
+    result = inference.get_most_probable_result()
+
+    assert 'TOP' == result
+
 
 def test_should_return_probability_distribution():
     inference = Inference(':LEFT-SIDE', ':BOTTOM', 'cup.n.01')
@@ -20,6 +25,11 @@ def test_should_return_probability_distribution():
     assert list == type(result)
 
     inference = Inference(':LEFT-SIDE', ':BOTTOM', 'bowl.n.01')
+    result = inference.get_storted_list_of_grasping_types_based_on_probability()
+
+    assert list == type(result)
+
+    inference = Inference(':LEFT-SIDE', ':BOTTOM', 'spoon.n.01')
     result = inference.get_storted_list_of_grasping_types_based_on_probability()
 
     assert list == type(result)
