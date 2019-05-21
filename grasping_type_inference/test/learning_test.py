76d3b3e2509d4b29382e66f3a0ba94c01f8b783f
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-05-21 11:04:03 +0200

    test(learning_test.py): Adds test case to handle mln parameter

--- grasping_type_inference/test/learning_test.py
+++ grasping_type_inference/test/learning_test.py
@@ -5,7 +5,12 @@ from grasping_type_inference.definitions import ROOT_DIR
 
 
 TRAINING_DATA_FILE_PATH = os.path.join(ROOT_DIR, 'mln')
+MLN_DATA_FILE_PATH = os.path.join(ROOT_DIR, 'test', 'mln')
 
 
 def test_should_train_all_mlns_successful():
     train_all_grasping_mlns(TRAINING_DATA_FILE_PATH)
+
+
+def test_should_train_all_mlns_successful_with_parameter():
+    train_all_grasping_mlns(TRAINING_DATA_FILE_PATH, MLN_DATA_FILE_PATH)
