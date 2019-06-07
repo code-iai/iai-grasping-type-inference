eb766021f6ca2659f24ebaab9c1668bb029e2821
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-06-07 13:55:49 +0200

    build(setup.py): Sets to 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grasping_type_inference",
-    version="1.0.0",
+    version="1.0.1",
     author="Sebastian Koralewski",
     author_email="seba@cs.uni-bremen.de",
     description="Utilizes a Markov Logic Network to infer the most probable grasping type for a defined object",
